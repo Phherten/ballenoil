3f886106d0f0cad94019d4783b020f5b159a7c61
Phherten <phigueraherten@gmail.com>
2022-09-28 12:08:13 +0200

    saludar

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask
-from funciones import euribor_scraping
+from flask import Flask, request
+from funciones import devolver_variables, agregar_datos
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['POST'])
+def agregar():  # put application's code here
+    request_body = request.get_json()
+    agregar_datos(request_body['km'])
+
+
+@app.route('/km')
+def km():
+    valor = devolver_variables()
+    return valor
 
 
 if __name__ == '__main__':
     app.run()
-
-
-@app.route('/saluda')
-def saluda():
-    valor = euribor_scraping()
-    return valor
-
--- funciones.py
+++ funciones.py
@@ -1,15 +1,15 @@
 import bs4
 import requests
 
-
-def euribor_scraping():
-    resultado = requests.get('https://www.expansion.com/mercados/euribor.html')
-    sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
-    valor = sopa.select('tbody tr td')[0]
-    euribor = valor.getText()
-    decimal = euribor.replace(',', '.')
-    return float(decimal)
+km_casa_almacen = []
+km_ruta = []
+km_ultima_casa = []
+conductor = []
 
 
+def agregar_datos(km):
+    km_ruta.append(km)
 
 
+def devolver_variables():
+    return km_ruta
